9e283f8b1b2fad156348b4e33b7f3ce40a2cc077
Ashfaque Hossain Abir <108783774+abirett5@users.noreply.github.com>
2023-09-21 12:56:45 +0600

    Files Added

---
+++ js-dom.js
@@ -0,0 +1,24 @@
+// Javascript Document Object Model (DOM)
+
+document.getElementById("id"); // Returns the element with id
+document.getElementsByTagName("tag"); // Returns the node list of elements with tag
+createElement(); // Create an element of the specified type 
+createTextNode(); // Create a text node from specified text 
+createAttribute(); // Create an attribute of the specified name 
+addEventListener(); // Attach an event handler of event to the document that runs function 
+
+// Mouse
+onclick = "";
+onmouseover = "";
+onmouseout = "";
+
+// Form 
+onchange
+onfocus
+onblur
+oninput
+
+// Keyboard
+onkeydown
+onkeyup
+onkeypress
